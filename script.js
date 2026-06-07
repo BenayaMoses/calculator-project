8d4c8b2df4b758d8e1a194cf56358b6fcc0aefa2
Ben <mosesbenaya11@gmail.com>
2026-06-07 23:47:49 +0700

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const display = document.getElementById('display');
 let shouldResetDisplay = false;
+
 function appendValue(value) {
-        if (display.value === '0' || shouldResetDisplay) {
+    if (display.value === '0' || shouldResetDisplay) {
         if (value !== '.') {
             display.value = '';
         }
@@ -9,9 +10,11 @@ function appendValue(value) {
     }
     display.value += value;
 }
+
 function clearDisplay() {
     display.value = '0';
 }
+
 function deleteLast() {
     if (display.value.length > 1) {
         display.value = display.value.slice(0, -1);
@@ -19,42 +22,15 @@ function deleteLast() {
         display.value = '0';
     }
 }
-function square() {
-    try {
-        display.value = Math.pow(eval(display.value), 2);
-    } catch {
-        display.value = 'Error';
-    }
-    shouldResetDisplay = true;
-}
-function sqrt() {
-    try {
-        display.value = Math.sqrt(eval(display.value));
-    } catch {
-        display.value = 'Error';
-    }
-    shouldResetDisplay = true;
-}
-function fraction() {
-    try {
-        display.value = 1 / eval(display.value);
-    } catch {
-        display.value = 'Error';
-    }
-    shouldResetDisplay = true;
-}
-function toggleSign() {
-    if (display.value !== '0' && display.value !== 'Error') {
-        if (display.value.startsWith('-')) {
-            display.value = display.value.slice(1);
-        } else {
-            display.value = '-' + display.value;
-        }
-    }
-}
+
 function calculate() {
     try {
         let expression = display.value;
+        
+        if (expression.includes('%')) {
+            expression = expression.replace(/%/g, '/100');
+        }
+
         let result = eval(expression);
         display.value = result;
         shouldResetDisplay = true;
